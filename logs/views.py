ac6385e889020026767b88a5922e39623634bc10
oopandas <oopandas@users.noreply.github.com>
2026-02-03 11:05:43 +0900

    Change urls.py in the config file, change admin.py, and change logs_list in views.py

--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import ChatLogModel
+
+def logs_view(request):
+    logs = ChatLogModel.objects.all()
+    return render(request, "logs/index.html", {"logs": logs})
+
+def logs_list(request):
+    logs = ChatLogModel.objects.all()
+    return render(request, "logs/logs_list.html", {"logs": logs})
 
 # Create your views here.
