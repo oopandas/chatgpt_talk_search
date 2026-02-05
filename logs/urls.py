38b65997926a5670896fe28c23df339a2d8a4e89
熊谷駿 <kumagaishun@kumagaishunnoMacBook-Air.local>
2026-02-06 08:12:11 +0900

    Create a view for logs_detail

--- logs/urls.py
+++ logs/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.logs_view, name="logs"),
     path("list/", views.logs_list, name="logs_list"),
+    path("detail/<int:pk>/", views.logs_detail, name="logs_detail")
 ]
--- logs/views.py
+++ logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ChatLogModel
 
 def logs_view(request):
@@ -8,5 +8,10 @@ def logs_view(request):
 def logs_list(request):
     logs = ChatLogModel.objects.all()
     return render(request, "logs/logs_list.html", {"logs": logs})
+
+def logs_detail(request, pk):
+    logs = get_object_or_404(ChatLogModel, pk=pk)
+    return render(request, "logs/logs_detail.html", {"logs": logs})
+
 
 # Create your views here.
